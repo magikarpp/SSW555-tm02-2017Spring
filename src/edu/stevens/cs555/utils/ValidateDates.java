07527b5e73cc718be20014f120e8dfccbef08099
KhushaliDave <kdave2@stevens.edu>
2017-02-27 12:05:07 -0500

    Added Logger with example to follow

--- src/edu/stevens/cs555/utils/ValidateDates.java
+++ src/edu/stevens/cs555/utils/ValidateDates.java
@@ -1,37 +1,36 @@
 package edu.stevens.cs555.utils;
 
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import edu.stevens.cs555.Family;
 
 public class ValidateDates {
 	
-	
+	 private static final Logger LOGGER = Logger.getLogger(ValidateDates.class.getName());
+	 
 	public void validateFamilyDates(Family fam) throws Exception{
 		Date rn = new Date();
 		
 		if(!Validate.noNulls(fam.getMarrDate())){
 			if(fam.getMarrDate().compareTo(rn) > 0){
-				System.out.println("");
-				throw new java.lang.Exception("Married is past current date.");
+				LOGGER.log(Level.SEVERE, "Married is past current date.");
 			}
 		}
 	
 		if(!Validate.noNulls(fam.getDivorceDate())){
 			if(fam.getDivorceDate().compareTo(rn) > 0){
-				System.out.println("");
-				throw new java.lang.Exception("Divorced is past current date.");
+				LOGGER.log(Level.SEVERE,"Divorced is past current date.");
 			}
 		}
 		
 		if(fam.getMarrDate() != null){
 			if(fam.getMarrDate().compareTo(fam.getHusband().getBirthDate()) < 0){
-				System.out.println("");
-				throw new java.lang.Exception("Birthdate of Husband is after Marriage");
+				LOGGER.log(Level.SEVERE,"Birthdate of Husband is after Marriage");
 			}
 			if(fam.getMarrDate().compareTo(fam.getWife().getBirthDate()) < 0){
-				System.out.println("");
-				throw new java.lang.Exception("Birthdate of Wife is after Marriage");
+				LOGGER.log(Level.SEVERE,"Birthdate of Wife is after Marriage");
 			}
 		}
 	
